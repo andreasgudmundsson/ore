40639cdc4e533a290d27ee4d973aac5679fd5e41
Andreas Gudmundsson <andreas.gudmundsson@booking.com>
2018-09-12 17:35:10 +0200

    WIP: streaming printer... asciigraph library crashes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/go-graphite/carbonapi/expr"
 	"github.com/go-graphite/carbonapi/expr/functions"
@@ -8,7 +9,11 @@ import (
 	"github.com/go-graphite/carbonapi/expr/types"
 	"github.com/go-graphite/carbonapi/pkg/parser"
 	pb "github.com/go-graphite/protocol/carbonapi_v3_pb"
-	//	ui "github.com/gizak/termui"
+	"math"
+	"os"
+	"strconv"
+
+	"github.com/guptarohit/asciigraph"
 )
 
 type graphRequest struct {
@@ -20,82 +25,96 @@ type graphRequest struct {
 	until    int64
 }
 
+func readLoop(do func(string)) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		do(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
+
 func main() {
-	ee := graphRequest{
-		metric:   "stdin",
-		request:  "derivative(stdin)",
-		from:     1437127020,
-		until:    1437127140,
-		values:   []float64{0, 1, 2, 4, 8, 16, 16, 16},
-		stepTime: 60,
+	userExpr := "stdin"
+	if len(os.Args) > 1 {
+		userExpr = os.Args[1]
 	}
 
-	rewrite.New(make(map[string]string))
-	functions.New(make(map[string]string))
-
-	exp, e, err := parser.ParseExpr(ee.request)
+	parsedExpr, e, err := parser.ParseExpr(userExpr)
 	if err != nil || e != "" {
 		fmt.Errorf("error='%v', leftovers='%v'", err, e)
 	}
 
+	var ts []float64
+	readLoop(
+		func(s string) {
+			x, err := strconv.ParseFloat(s, 64)
+			if err == nil {
+				ts = append(ts, x)
+				if len(ts) > 60*60 {
+					ts = ts[len(ts)>>1:]
+				}
+				plot(parsedExpr, ts)
+			}
+		},
+	)
+}
+
+func plot(exp parser.Expr, ts []float64) {
+	metric := "stdin"
+	from := int64(0)
+	until := int64(len(ts))
+
 	metricData := types.MetricData{
 		FetchResponse: pb.FetchResponse{
-			Name:              ee.metric,
-			StartTime:         ee.from,
-			StopTime:          ee.until,
-			StepTime:          ee.stepTime,
-			Values:            ee.values,
+			Name:              metric,
+			StartTime:         from,
+			StopTime:          until,
+			StepTime:          1,
+			Values:            ts,
 			ConsolidationFunc: "average",
-			PathExpression:    ee.metric,
+			PathExpression:    metric,
 		},
 	}
 
 	metricMap := make(map[parser.MetricRequest][]*types.MetricData)
 	{
 		request := parser.MetricRequest{
-			Metric: ee.metric,
-			From:   ee.from,
-			Until:  ee.until,
+			Metric: metric,
+			From:   from,
+			Until:  until,
 		}
 		metricMap[request] = []*types.MetricData{
 			&metricData,
 		}
 	}
 
-	out, err := expr.EvalExpr(exp, ee.from, ee.until, metricMap)
+	rewrite.New(make(map[string]string))
+	functions.New(make(map[string]string))
+	out, err := expr.EvalExpr(exp, from, until, metricMap)
 	if err != nil {
 		fmt.Errorf("error='%v' expr='%v'", err, exp)
 	}
-	fmt.Printf("exp: %v\n", exp)
-	fmt.Printf("metricMap: %v\n", out)
-
-	/*
-
-		err := ui.Init()
-		if err != nil {
-			panic(err)
+	fmt.Println(metricMap)
+	fmt.Println(out)
+	if len(out) > 0 {
+		values := NaNToZero(out[0].Values)
+		if len(values) > 1 {
+			fmt.Println(values)
+			graph := asciigraph.Plot(values)
+			fmt.Println(graph)
 		}
-		defer ui.Close()
+	}
+}
 
-
-
-			lc0 := ui.NewLineChart()
-			lc0.BorderLabel = "braille-mode Line Chart"
-			lc0.Data = metricData.Values
-			lc0.Width = 600
-			lc0.Height = 600
-			lc0.X = 0
-			lc0.Y = 0
-			lc0.AxesColor = ui.ColorWhite
-			lc0.LineColor = ui.ColorGreen | ui.AttrBold
-
-			ui.Render(lc0)
-
-			ui.Handle("q", func(ui.Event) {
-				ui.StopLoop()
-			})
-
-			//	ui.Loop()
-	*/
-
+func NaNToZero(xs []float64) []float64 {
+	var ys []float64
+	for _, x := range xs {
+		if math.IsNaN(x) {
+			x = 0
+		}
+		ys = append(ys, x)
+	}
+	return ys
 }
